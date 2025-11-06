dc8716b941ea038eb830a72f19805fcf9626b2ce
Willem Schipper <git@willem.page>
2025-11-06 13:33:31 +0100

    Grammateus classifier: ensure same distribution in test, dev, train Add executable permission to training script

--- grammateus_classification/01_prepare_data.py
+++ grammateus_classification/01_prepare_data.py
@@ -11,6 +11,8 @@ from tqdm import tqdm
 
 # TODO reproducible test/dev/train sets, plus ensure correct distribution in test/dev/train?
 
+GRAMMATEUS_TYPES = ["Epistolary Exchange", "Objective Statement", "Recording of Information", "Transmission of Information"]
+
 def main():
     mongo_database = environ.get("MONGO_DATABASE", "papyri")
     mongo_collection = environ.get("MONGO_COLLECTION", "maat")
@@ -28,14 +30,18 @@ def main():
     types_and_classes_preprocessed = [(preprocess(terms), grammateus_type)  
                 for terms, grammateus_type in types_and_classes]
    
+    train_data = []
+    dev_data = []
+    test_data = []
 
-    random.Random(42).shuffle(types_and_classes_preprocessed)
 
-    split_idx_test = int(len(types_and_classes_preprocessed) * 0.1)
-    split_idx_dev = int(len(types_and_classes_preprocessed) * 0.2)
-    train_data = types_and_classes_preprocessed[split_idx_dev:]
-    dev_data = types_and_classes_preprocessed[split_idx_test:split_idx_dev]
-    test_data = types_and_classes_preprocessed[:split_idx_test]
+    # ensure equal distribution in train, dev and test sets
+    for grammateus_type in GRAMMATEUS_TYPES:
+        types_and_classes_single_type = [type_and_classes for type_and_classes in types_and_classes_preprocessed if type_and_classes[1] == grammateus_type]
+        train, dev, test = test_dev_train_split(types_and_classes_single_type)
+        train_data.extend(train)
+        dev_data.extend(dev)
+        test_data.extend(test)
 
 
     print(f"Total: {len(types_and_classes_preprocessed)} - Train:  {len(train_data)} - Dev: {len(dev_data)} - Test: {len(test_data)}")
@@ -43,12 +49,24 @@ def main():
     write_to_disk(dev_data, "dev.spacy", nlp)
     write_to_disk(test_data, "test.spacy", nlp)
 
+
+def test_dev_train_split(data_list, random_seed=42):
+
+    random.Random(random_seed).shuffle(data_list)
+
+    split_idx_test = int(len(data_list) * 0.1)
+    split_idx_dev = int(len(data_list) * 0.2)
+    train_data = data_list[split_idx_dev:]
+    dev_data = data_list[split_idx_test:split_idx_dev]
+    test_data = data_list[:split_idx_test]
+
+    return train_data, dev_data, test_data
+
 def write_to_disk(data, filename, nlp: Language):
     db = DocBin()
 
-    types = ["Epistolary Exchange", "Objective Statement", "Recording of Information", "Transmission of Information"]
     for doc, label in nlp.pipe(data, as_tuples=True):
-        for grammateus_type in types:
+        for grammateus_type in GRAMMATEUS_TYPES:
             doc.cats[grammateus_type] = label == grammateus_type
         db.add(doc)
     db.to_disk(filename)
