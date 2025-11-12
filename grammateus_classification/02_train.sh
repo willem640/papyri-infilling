7213167d7526708fa12707b53773a7ab78bef1bd
Willem Schipper <git@willem.page>
2025-11-12 12:53:38 +0100

    Add automatic evaluation to training script

--- grammateus_classification/02_train.sh
+++ grammateus_classification/02_train.sh
@@ -1,1 +1,7 @@
 python -m spacy train config_spacy_train.cfg --output model --verbose -g 0
+
+echo "Eval best:"
+spacy evaluate model/model-best/ test.spacy -g 0 -o model/eval-best.json | sed -e 's/\x1b\[[0-9;]*m//g' | tee model/eval-best.txt
+echo "Eval last:"
+spacy evaluate model/model-last/ test.spacy -g 0 -o model/eval-last.json | sed -e 's/\x1b\[[0-9;]*m//g' | tee model/eval-last.txt
+cp test.spacy model/
