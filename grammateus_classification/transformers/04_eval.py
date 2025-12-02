fc3537be34017acfad0f71d6b517f9340942b35f
Willem Schipper <git@willem.page>
2025-12-02 12:11:50 +0100

    First version of eval script

--- grammateus_classification/transformers/04_eval.py
+++ grammateus_classification/transformers/04_eval.py
@@ -1,1 +1,54 @@
-#TODO
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding
+from datasets import load_dataset
+import evaluate
+import numpy as np
+
+MODEL_PATH = "models/model_concat_best/checkpoint-1000"
+GRAMMATEUS_TYPES = ["Epistolary Exchange", "Transmission of Information", "Objective Statement", "Recording of Information"]
+
+def main():
+    data = load_dataset("json", data_files={'test':'test.json'})
+
+    tokenizer = AutoTokenizer.from_pretrained('bowphs/GreBerta')
+
+    tokenized_data = data.map(lambda papyrus: tokenizer(papyrus['text'], truncation=True, max_length=512), batched=True)
+
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    label2id = {label: GRAMMATEUS_TYPES.index(label) for label in GRAMMATEUS_TYPES}
+    id2label = {idno: label for label, idno in label2id.items()}
+
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH, num_labels=len(GRAMMATEUS_TYPES), id2label=id2label, label2id=label2id)
+
+    tokenized_data = tokenized_data.remove_columns(data["test"].column_names)
+
+    loader = torch.utils.data.DataLoader(tokenized_data['test'].with_format('torch'), batch_size=32, collate_fn=data_collator) 
+
+
+    predictions = []
+    for batch in loader:
+        predictions_batch = model(**batch)
+        predictions.extend(predictions_batch)
+
+    for prediction in predictions:
+        print(prediction)
+        exit()
+
+    metrics = compute_metrics(predictions)
+
+    print(f"Eval: {metrics}")
+
+
+
+
+
+metric=evaluate.load('f1')
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+
+    return metric.compute(predictions=predictions, references=labels, average='macro')
+
+if __name__=="__main__":
+    main()
