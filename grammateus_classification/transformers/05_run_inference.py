9563b92ee622b41eb6fb5b70ba72bd46e72f02e7
Willem <git@willem.page>
2025-12-02 16:55:48 +0100

    Grammateus: fix inference script for transformers

--- grammateus_classification/transformers/05_run_inference.py
+++ grammateus_classification/transformers/05_run_inference.py
@@ -3,9 +3,13 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, Data
 from datasets import load_dataset
 import evaluate
 import numpy as np
+from tqdm import tqdm
+import json
 
 MODEL_PATH = "model/checkpoint-1000"
 GRAMMATEUS_TYPES = ["Epistolary Exchange", "Transmission of Information", "Objective Statement", "Recording of Information"]
+
+device = 'cuda'
 
 def main():
     data = load_dataset("json", data_files={'data':'all_unlabeled.json'})
@@ -14,22 +18,24 @@ def main():
 
     tokenized_data = data.map(lambda papyrus: tokenizer(papyrus['text'], truncation=True, max_length=512), batched=True)
 
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, padding=True)
 
     label2id = {label: GRAMMATEUS_TYPES.index(label) for label in GRAMMATEUS_TYPES}
     id2label = {idno: label for label, idno in label2id.items()}
 
-    model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH, num_labels=len(GRAMMATEUS_TYPES), id2label=id2label, label2id=label2id)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH, num_labels=len(GRAMMATEUS_TYPES), id2label=id2label, label2id=label2id, device_map='auto')
 
-    tokenized_data = tokenized_data.remove_columns(data["data"].column_names)
+    tokenized_data = tokenized_data.remove_columns(data['data'].column_names)
+    print(tokenized_data)
 
-    loader = torch.utils.data.DataLoader(tokenized_data['data'].with_format('torch'), batch_size=32, collate_fn=data_collator) 
+    loader = torch.utils.data.DataLoader(tokenized_data['data'], batch_size=32, collate_fn=data_collator) 
 
 
     predictions = []
-    for batch in loader:
+    for batch in tqdm(loader):
+        batch = {k: v.to(device) for k, v in batch.items()}   
         predictions_batch = model(**batch)
-        predictions.extend(predictions_batch['logits'])
+        predictions.extend(predictions_batch['logits'].detach().cpu())
 
 
     predictions = [get_cat(tensor.cpu().detach(), id2label) for tensor in predictions]
@@ -37,13 +43,13 @@ def main():
 
     all_papyri_processed = [{'grammateus_type': grammateus_type, **papyrus} for grammateus_type, papyrus in zip(predictions, list(data['data']['meta']))]
 
-    with open('papyri_with_classes', 'w') as f:
+    with open('papyri_with_classes.json', 'w') as f:
         json.dump(all_papyri_processed, f)
 
 
 
 def get_cat(prediction, id2label):
-    cat = np.argmax(prediction)
+    cat = np.argmax(prediction).item()
     return id2label[cat]
 
 if __name__=="__main__":
