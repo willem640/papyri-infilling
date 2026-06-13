72cab95dd6ce511faaac4c59b732a8a57cda4733
Willem Schipper <git@willem.page>
2026-06-13 17:47:38 +0200

    Add WIP data augmentation script

--- grammateus_classification/spacy/helper_scripts/data_augmentation.py
+++ grammateus_classification/spacy/helper_scripts/data_augmentation.py
@@ -13,3 +13,9 @@ def randomly_remove_characters(text: str, p: float):
 
     return ''.join(characters)
 
+def random_corrupt_segments(text: str, p_per_segment: float, percent_of_segment: float, contiguous_in_segment: bool):
+    pass
+
+# adapted from: https://github.com/ericu9500/PapyriAndInscriptions/blob/master/train_data/12_prepare_train_text_2.py
+def segment(text: str):
+    pass
